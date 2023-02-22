08b090b08f9e603371a4745cd4eef3af7653f6a9
Ryan Penley <ryanpenley@gmail.com>
2023-02-22 12:12:08 -0500

    got amort table to work

--- js/site.js
+++ js/site.js
@@ -2,81 +2,136 @@
 
 //loanAmount, termAmount, interestRate
 function getValues() {
-    let loanAmount = document.getElementById('loanAmount').value;
-    let termLength = document.getElementById('termAmount').value;
-    let interestRate = document.getElementById('interestRate').value;
+  let loanAmount = parseFloat(document.getElementById('loanAmount').value);
+  let termLength = parseInt(document.getElementById('termAmount').value);
+  let interestRate = parseFloat(document.getElementById('interestRate').value);
 
-    let mathStuff = loanCalculation(loanAmount, termLength, interestRate);
+  loanCalculation(loanAmount, termLength, interestRate);
 
-    displayResults(mathStuff);
+  // displayResults(mathStuff);
 }
 
 // logic function
 function loanCalculation(loanAmount, termInput, interestInput) {
-   let loanInput = parseFloat(loanAmount);
-   let termLength = parseFloat(termInput);
-   let interestRate = parseFloat(interestInput);
+  let loanInput = loanAmount;
+  let termLength = termInput;
+  let interestRate = interestInput;
 
 
 
 
-    let totalMonthlyPayment = (loanInput*(interestRate/1200))/(1-(1+interestRate/1200)**-termLength);
-    let remainingBalance = loanInput;
-    let interestPayment = remainingBalance * interestRate/1200;
-    let principal = totalMonthlyPayment - interestPayment;
-    let totalInterest = interestPayment;
+  let totalMonthlyPayment = (loanInput * (interestRate / 1200)) / (1 - (1 + interestRate / 1200) ** -termLength);
+  let remainingBalance = loanInput;
+  let interestPayment = remainingBalance * interestRate / 1200;
+  let principal = totalMonthlyPayment - interestPayment;
+  let totalInterest = interestPayment;
 
-    let monthlyPayments = [];
+  let monthlyPayments = [];
 
 
-    for(let i = 0; i <= termLength; i++) {
+  for (let i = 0; i <= termLength; i++) {
 
-        interestPayment = remainingBalance * interestRate/1200;
-        principal = totalMonthlyPayment-interestPayment;
-        totalInterest += interestPayment;
-        remainingBalance -= principal;
-        let monthlyPaymentData = {
-            month: i,
-            payment: totalMonthlyPayment,
-            principal: principal,
-            interest: interestPayment,
-            totalInterest: totalInterest,
-            balance: remainingBalance
-        }
-        monthlyPayments.push(monthlyPaymentData);
-    } 
-    
-    return monthlyPayments;
+    interestPayment = remainingBalance * (interestRate / 1200);
+    principal = totalMonthlyPayment - interestPayment;
+    totalInterest += interestPayment;
+    remainingBalance -= principal;
+    let monthlyPaymentData = {
+      month: i,
+      payment: totalMonthlyPayment,
+      principal: principal,
+      interest: interestPayment,
+      totalInterest: totalInterest,
+      balance: remainingBalance
+    }
+    monthlyPayments.push(monthlyPaymentData);
+  }
+
+  return displayResults(monthlyPayments);
 
 }
 
 
-    function displayResults(loanData) {
+function displayResults(loanPayments) {
   //Show results
-// top card results
-let principalElementValue = document.getElementById("loanAmount").value;
-let totalInterestElementValue = loanData[loanData.length - 1].totalInterest;
+  // top card results
+  let principalElementValue = Number(document.getElementById("loanAmount").value);
+  let totalInterestElementValue = loanPayments[loanPayments.length - 1].totalInterest;
+  let totalCostValue = Number(principalElementValue + totalInterestElementValue);
 
 
-document.getElementById("MonthlyPaymentAmountDisplay").textContent = loanData[0].payment.toLocaleString('en-US', {style:'currency',currency:'USD'});
 
-document.getElementById("principal").textContent = principalElementValue.toLocaleString('en-US', {style:'currency',currency:'USD'});
-
-document.getElementById("totalInterestDisplay").textContent = totalInterestElementValue.toLocaleString('en-US', {style:'currency',currency:'USD'});
-
-document.getElementById("totalCostDisplay").textContent = (principalElementValue + totalInterestElementValue).toLocaleString('en-US', {style:'currency',currency:'USD'});
+  // amort table
+  let tableBody = document.getElementById('loanTableBody');
+  const tableRowTemplate = document.getElementById('mortgageTableRowTemplate');
 
 
-//   table results
-  document.getElementById("monthDisplay").innerHTML = "" + month;
+  for (let i = 0; i < loanPayments.length - 1; i++) {
 
-  document.getElementById("monthlyPaymentDisplay").innerHTML = "$";
+    let eventRow = document.importNode(tableRowTemplate.content, true);
+    let tableCells = eventRow.querySelectorAll('td');
 
-  document.getElementById("paymentAmountDisplay").innerHTML = "$";
 
-  document.getElementById("totalInterest").innerHTML = "%";
 
-  document.getElementById("totalPayment").innerHTML = "$";
+    // month
+    tableCells[0].innerHTML = i + 1;
+
+    // payment
+    tableCells[1].innerHTML = formatCurrency(loanPayments[i].payment);
+
+    // principal
+    tableCells[2].innerHTML = formatCurrency(loanPayments[i].principal);
+
+    // interest
+    tableCells[3].innerHTML = formatCurrency(loanPayments[i].interest);
+
+    // total interest
+    tableCells[4].innerHTML = formatCurrency(loanPayments[i].totalInterest);
+
+    // balance
+    // use math.abs to show absolute value of amount instead of negative number
+    tableCells[5].innerHTML = formatCurrency(Math.abs(loanPayments[i].balance));
+
+    tableBody.appendChild(eventRow);
+  }
+
+  // let totalInterest = loanPayments[loanPayments.length - 1].totalInterest;
+  // let totalCost = totalInterest + principal;
+  // let monthlyPayment = loanPayments[0].monthlyPayment
+
+  document.getElementById("MonthlyPaymentAmountDisplay").textContent = formatCurrency(loanPayments[0].payment);
+
+  document.getElementById("principal").textContent = formatCurrency(principalElementValue);
+
+  document.getElementById("totalInterestDisplay").textContent = formatCurrency(totalInterestElementValue);
+
+  document.getElementById("totalCostDisplay").textContent = formatCurrency(totalCostValue);
+
+
+
+
+
+  function formatCurrency(value) {
+    return Number(value).toLocaleString('en-us', {
+      style: 'currency',
+      currency: 'USD'
+    });
+  }
+
+
+
+
+
+
+  // //   table results
+  //   document.getElementById("monthDisplay").innerHTML = "" + month;
+
+  //   document.getElementById("monthlyPaymentDisplay").innerHTML = "$";
+
+  //   document.getElementById("paymentAmountDisplay").innerHTML = "$";
+
+  //   document.getElementById("totalInterest").innerHTML = "%";
+
+  //   document.getElementById("totalPayment").innerHTML = "$";
 
 
 
